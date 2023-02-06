d58a4d520b8b0f3103e89c0000e27aede4454c01
agungf97 <mohamadagungfaisal@gmail.com>
2023-02-06 15:48:42 +0700

    #

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { BsFillSunFill } from 'react-icons/bs';
 import { useState } from "react";
-import Header from './components/navbar.js';
-import Home from './components/home.js';
-import About from './components/about.js';
-import Experience from './components/experience.js';
-import Portofolio from './components/portofolio.js';
-import Contact from './components/contact.js';
-import Footer from './components/footer.js';
-import Sosmed from './components/sosmed.js';
+import Navbar from './components/navbar/navbar.js';
+import Home from './components/home/home.js';
+import About from './components/about/about.js';
+import Experience from './components/experience/experience.js';
+import Portofolio from './components/portofolio/portofolio.js';
+import Contact from './components/contact/contact.js';
+import Footer from './components/footer/footer.js';
+import Sosmed from './components/sosmed/sosmed.js';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className={darkMode ? "dark" : ""}>
       <main className="dark:bg-slate-900">
-        <Header/>
+        <Navbar/>
         <div className="container">
           <div className="flex justify-between dark:text-white">
             <Sosmed/>
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -11,11 +11,11 @@ const Navbar = () => {
                         <img className="w-24 hover:scale-100 hover:opacity-90" src={maf} alt=""/>
                     </div>
                     <div>
-                        <ol className="md:flex md:space-x-8 text-xl">
-                            <li className="font-bold hidden md:block hover:font-semibold my-auto align-middle">About Me</li>
-                            <li className="font-bold hidden md:block hover:font-semibold my-auto align-middle">Experience</li>
-                            <li className="font-bold hidden md:block hover:font-semibold my-auto align-middle">Portofolio</li>
-                            <li className="font-bold hidden md:block hover:font-semibold my-auto align-middle">Contact</li>
+                        <ol className="md:flex md:space-x-8 text-lg">
+                            <li className="hidden md:block hover:font-semibold my-auto align-middle">About Me</li>
+                            <li className="hidden md:block hover:font-semibold my-auto align-middle">Experience</li>
+                            <li className="hidden md:block hover:font-semibold my-auto align-middle">Portofolio</li>
+                            <li className="hidden md:block hover:font-semibold my-auto align-middle">Contact</li>
                             <button className="text-center font-bold hidden md:block border-4 rounded-full px-6 align-middle border-orange-500 dark:border-orange-700 hover:border-orange-700 hover:text-white hover:bg-orange-700 duration-500">Resume</button>
                             <div className="block md:hidden">
                                 <Bars3Icon className="h-auto w-8"/>
